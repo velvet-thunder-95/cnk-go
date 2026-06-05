fbdcafed542d55408497aa4c7562f928433980a6
Velvet Thunder <abhishekkur125@gmail.com>
2026-06-05 20:08:56 +0530

    fix linting

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 
 export default [
-    { ignores: ['node_modules/**'] },
+    { ignores: [ 'node_modules/**' ] },
     js.configs.recommended,
     {
         languageOptions: {
